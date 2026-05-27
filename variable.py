15a09acf7358dc4479655c5473782cf6eb0d45b5
IshanSingh9273 <singhishan1103@gmail.com>
2026-05-27 22:39:59 +0530

    variable in python

---
+++ variable.py
@@ -0,0 +1,48 @@
+# VARIABLE IN PYTHON
+
+#STRING VARIABLE
+name = "Ishan Singh "
+
+# integer variable 
+age = 20 
+
+# float variable 
+
+height = 183.0
+
+
+
+
+print(name)
+print(age)
+print(height)
+
+
+
+# checking data type 
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+# basic maths operation 
+
+x = 66
+y = 16
+z = 18
+
+print(x*y*z)
+print(x+y+z)
+print(x/y)
+
+
+
+# user input 
+
+user_name = input("enter your name")
+
+print("hello ", user_name)
+
+
+
